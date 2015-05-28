3ede87b83335959c1d7d2a58db740c800bdf671b
RLijewski <pxihcky35@Ryans-MacBook-Pro.local>
2015-05-28 18:33:50 -0500

    Initial Commit

---
+++ US Capitals Quiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  US Capitals Quiz
+//
+//  Created by Ryan Lijewski on 5/28/15.
+//  Copyright (c) 2015 Developed By Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
