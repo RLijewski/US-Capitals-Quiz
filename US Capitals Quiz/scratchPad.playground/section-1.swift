c5b7f89233aaee87937a592fb65e75cb5f538966
Ryan Lijewski <RLijewski@users.noreply.github.com>
2016-02-28 08:12:58 -0600

    Added the rest of the project files

---
+++ US Capitals Quiz/scratchPad.playground/section-1.swift
@@ -0,0 +1,16 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+import UIKit
+
+var array = ["cheese", "bread"]
+
+var item = array.removeAtIndex(1)
+
+item
+
+var enabled: false
+
+
+
+
